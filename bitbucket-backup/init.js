76c6b500042ccca8f253b1779660360a7049860a
Allan Taschini <allan.taschini@gmail.com>
2019-06-14 02:43:20 +0200

    Fix branch switching

--- bitbucket-backup/init.js
+++ bitbucket-backup/init.js
@@ -29,16 +29,27 @@ shell.cat(args[0])
       shell.exec(`git clone --progress git@bitbucket.org:${url}.git`)
     }
     shell.cd(repoFolder)
-    shell.exec(`git branch -r`)
-      .grep('-v', 'HEAD')
-      .grep('-v', 'master')
+    let remoteBranches = shell.exec(`git branch -r`)
       .split(EOL)
       .filter(branch => branch.length)
-      .forEach(branch => {
-        shell.exec(`git checkout --force --track ${branch}`)
-      })
-    shell.exec('git fetch --all --tags --prune')
-    shell.exec('git checkout --force master')
+    if (remoteBranches.length !== 0) {
+      let headBranch = remoteBranches
+        .find(branch => branch.includes('HEAD'))
+        .split(' -> ')[1]
+      let headBranchLocal = headBranch.split('/')[1]
+      let targetBranches = remoteBranches
+        .filter(branch => !branch.includes(headBranch))
+        .map(branch => branch.trim())
+
+      targetBranches.forEach(branch => {
+          shell.exec(`git checkout --force --track ${branch}`)
+        })
+
+      shell.exec('git fetch --all --tags --prune')
+      shell.exec(`git checkout --force ${headBranchLocal}`)
+    } else {
+      shell.echo('No remote branches to work with...')
+    }
     shell.cd(cwd)
 
     shell.echo('')
