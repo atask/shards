a9f33dcd89c7e5d0374465f208cc131a96abb401
Allan <allan.taschini@gmail.com>
2019-07-24 02:28:29 +0200

    Trivial refactors

--- whatsapp-puppeteer/index.js
+++ whatsapp-puppeteer/index.js
@@ -1,8 +1,46 @@
 const puppeteer = require('puppeteer')
 
+async function waitForWAConnection(page) {
+  const client = page._client
+  return new Promise(resolve => {
+    client.on(
+      'Network.webSocketFrameReceived',
+      ({ requestId, timestamp, response }) => {
+        if (response.payloadData.includes('"connected":true')) {
+          resolve()
+        }
+      }
+    )
+  })
+}
+
+async function waitForWADisconnection(page) {
+  const client = page._client
+  return new Promise(resolve => {
+    client.on(
+      'Network.webSocketFrameReceived',
+      ({ requestId, timestamp, response }) => {
+        if (response.payloadData.includes('["Cmd",{"type":"disconnect"}]')) {
+          resolve()
+        }
+      }
+    )
+  })
+}
+
+// const CHAT_SELECTOR = '.X7YrQ'
+const CHAT_SELECTOR = 'body'
+async function getChats(page) {
+  return await page.$$eval(CHAT_SELECTOR, getChatDetails);
+}
+
+async function getChatDetails() {
+  console.log('here')
+}
+
 ;(async () => {
   const browser = await puppeteer.launch({
-    args: [ '--proxy-pac-url=http://wpad.reply.it/wpad.dat' ],
+    //args: [ '--proxy-pac-url=http://wpad.example.it/wpad.dat' ],
     headless: false,
     slowMo: 250
   })
@@ -10,31 +48,6 @@ const puppeteer = require('puppeteer')
   const page = await browser.newPage()
 
   const client = page._client
-
-  // client.on('Network.webSocketCreated', ({ requestId, url }) => {
-  //   console.log('Network.webSocketCreated', requestId, url)
-  // })
-
-  // client.on('Network.webSocketClosed', ({ requestId, timestamp }) => {
-  //   console.log('Network.webSocketClosed', requestId, timestamp)
-  // })
-
-  // client.on('Network.webSocketFrameSent', ({ requestId, timestamp, response }) => {
-  //   console.log('Network.webSocketFrameSent', requestId, timestamp, response.payloadData)
-  // })
-
-  client.on('Network.webSocketFrameReceived', async ({ requestId, timestamp, response }) => {
-    // console.log('Network.webSocketFrameReceived', requestId, timestamp, response.payloadData)
-
-    if (response.payloadData.includes('"connected":true')) {
-      console.log('connected!')
-    }
-
-    if (response.payloadData.includes('["Cmd",{"type":"disconnect"}]')) {
-      console.log('disconnected!')
-      await browser.close()
-    }
-  })
 
   await page.goto(
     'https://web.whatsapp.com/',
@@ -44,6 +57,16 @@ const puppeteer = require('puppeteer')
   const rememberMeSelector = 'input[name="rememberMe"]'
   await page.waitForSelector(rememberMeSelector)
   await page.click(rememberMeSelector)
+
+  await waitForWAConnection(page)
+  console.log('connected!')
+
+  let chats = await getChats(page);
+
+  await waitForWADisconnection(page)
+  console.log('disconnected!')
+  await browser.close()
 })().catch(e => {
   // Deal with the fact the chain failed
+  console.error('ERROR!', e)
 })
